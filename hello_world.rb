7062d569b7fb98998d55262ab10db30de8eada1d
Amanda Rolon <abrolon87@gmail.com>
2021-05-31 23:40:27 +0000

    Automatically backed up by Learn

---
+++ hello_world.rb
@@ -0,0 +1,19 @@
+likely_to_rain = TRUE
+garment = likely_to_rain ? "galoshes" : "sun hat"
+
+def say_hello_world_five_times
+  message = "Hello World!"
+  puts message
+  puts message
+  puts message
+  puts message
+  puts message
+end
+
+say_hello_world_five_times
+
+def say_hello_world
+  puts "Hello World!"
+end
+
+say_hello_world
---
+++ learning-comments.rb
@@ -0,0 +1,2 @@
+puts (1 + 1) #2
+puts "Joy to you and me"
